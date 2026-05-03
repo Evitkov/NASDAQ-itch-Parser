bcdf59cb526a200bc75753a6a3d854e9108586a5
Evitkov <lvalenta@tudelft.nl>
2026-05-03 20:33:46 +0200

    market updated for additional functinality as previous orderbook commit + total share counting for additional trade and cross trade message for debugging

--- include/Market.h
+++ include/Market.h
@@ -16,9 +16,29 @@ private:
     //We use array for locating orderbook for each stock by locate - fast lookups and no hashing math needed
     std::vector<OrderBook> books;
 
+    //For correctness checks
+    uint64_t total_volume = 0;
+
 public:
     Market();
 
+    // method to check total volume of all shares executed for debugging
+    uint64_t get_total_volume() const { return total_volume; }
+
     void process_stock_directory(const StockDirectoryMessage& msg);
     void process_add_order(const AddOrderMessage& msg);
+
+    void process_cancel_order(const OrderCancelMessage& msg) ;
+
+    void process_execute_order(const OrderExecutedMessage& msg) ;
+
+    void process_execute_with_price(const OrderExecutedWithPriceMessage& msg) ;
+
+    void process_trade(const TradeMessage& msg) ;
+
+    void process_cross_trade(const CrossTradeMessage& msg) ;
+
+    void process_delete_order(const OrderDeleteMessage& msg) ;
+
+    void process_replace_order(const OrderReplaceMessage& msg) ;
 };
--- src/Market.cpp
+++ src/Market.cpp
@@ -13,3 +13,36 @@ Market::Market() {
 void Market::process_add_order(const AddOrderMessage& msg) {
     books[msg.locate].process_add_order(msg);
 }
+void Market::process_stock_directory(const StockDirectoryMessage& msg) {
+    tickers[msg.locate] = msg.stock;
+    ticker_to_locate[msg.stock] = msg.locate;
+}
+void Market::process_cancel_order(const OrderCancelMessage& msg) {
+    books[msg.locate].process_cancel_order(msg);
+}
+
+void Market::process_execute_order(const OrderExecutedMessage& msg) {
+    books[msg.locate].process_execute_order(msg);
+    total_volume += msg.executed_shares;
+}
+
+void Market::process_execute_with_price(const OrderExecutedWithPriceMessage& msg) {
+    books[msg.locate].process_execute_with_price(msg);
+    total_volume += msg.executed_shares;
+}
+
+void Market::process_delete_order(const OrderDeleteMessage& msg) {
+    books[msg.locate].process_delete_order(msg);
+}
+
+void Market::process_replace_order(const OrderReplaceMessage& msg) {
+    books[msg.locate].process_replace_order(msg);
+}
+void Market::process_trade(const TradeMessage& msg) {
+    total_volume += msg.shares;
+
+}
+void Market::process_cross_trade(const CrossTradeMessage& msg) {
+    total_volume += msg.shares;
+
+}
